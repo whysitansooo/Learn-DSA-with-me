bc96d5a85fed38a83bfacdf1c5d00f36bbaf2f68
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2023-07-03 22:26:02 +0530

    All basics about pointers

---
+++ L23Pointers.cpp
@@ -0,0 +1,55 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int i = 10;
+//     cout<<&i<<endl;
+//     //! Pointers are variables which can store address of variables
+
+//     int * p = &i;
+//     cout<<p<<endl;
+//     (*p)++;
+//     cout<<*p<<endl; //! Dereference operator
+
+//     cout<<sizeof(p)<<endl;
+
+//     int *n = 0; //! NULL Pointer
+//     cout<<n<<endl;
+
+// }
+
+
+//? Predict Ouput 1
+
+#include <iostream>
+using namespace std;
+int main(){
+    // int a = 100,b=200;
+    // int *p=&a,*q=&b;
+    // p = q;
+    // cout<<p<<endl;
+    // // cout<<a<<endl;
+    // cout<<q<<endl;
+    // // cout<<b<<endl;
+    
+
+    // int a = 50;
+    // int *ptr = &a;
+    // cout<<(*ptr)++<<" ";
+    // cout<<a<<endl;
+
+
+    // int *ptr=0;
+    // int a = 10;
+    // *ptr = a;
+    // cout<<*ptr<<endl;
+
+    // float f = 10.5;
+    // float p = 2.5;
+    // float *ptr = &f;
+    // (*ptr)++; //* f = 11.5
+    // *ptr = p;
+    // cout << *ptr <<" " << f << " " << p;
+
+
+}
