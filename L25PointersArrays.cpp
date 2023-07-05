795c3f49f8202b7bfc0e3e0670b7d64787def5da
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2023-07-05 19:55:53 +0530

    Pointers & Arrays

--- L25PointersArrays.cpp
+++ L25PointersArrays.cpp
@@ -1,49 +1,49 @@
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     // int arr[10];
-//     // cout<<arr<<endl;
-//     // cout<<&arr<<endl;
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int arr[10];
+    // cout<<arr<<endl;
+    // cout<<&arr<<endl;
 
-//     // int *p = arr;
-//     // arr[0] = 5;
-//     // arr[1] = 10;
-//     // cout<<*arr<<endl;
-//     // cout<<*(arr+1)<<endl;
-//     // cout<<0[arr]<<endl;   
+    // int *p = arr;
+    // arr[0] = 5;
+    // arr[1] = 10;
+    // cout<<*arr<<endl;
+    // cout<<*(arr+1)<<endl;
+    // cout<<0[arr]<<endl;   
 
-//     // cout<<&p<<endl;
-//     // cout<<arr<<endl; 
+    // cout<<&p<<endl;
+    // cout<<arr<<endl; 
 
-// }
+}
 
  //? Sum of the Numbers
 
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int sum(int *ar,int n){
-//     int sum = 0;
-//     for(int i=0;i<n;i++){
-//         sum = sum + *ar;
-//         ar++;
-//     }
-//     return sum;
-// }
+int sum(int *ar,int n){
+    int sum = 0;
+    for(int i=0;i<n;i++){
+        sum = sum + *ar;
+        ar++;
+    }
+    return sum;
+}
 
-// int main(){
-//     int n;
-//     cin>>n;
-//     int ar[n];
-//     for(int i=0;i<n;i++)
-//     {
-//         cin>>ar[i];
-//     }
-//     int ans = sum(ar,n);
-//     cout<<ans;
-// }
+int main(){
+    int n;
+    cin>>n;
+    int ar[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>ar[i];
+    }
+    int ans = sum(ar,n);
+    cout<<ans;
+}
 
 //? Predict Output
 #include<iostream>
